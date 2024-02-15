dff350fffc9b464cabf7905a5ef821753d5744dd
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-15 15:56:05 +0530

    [Ajay] Add Having Clause

--- ms.sql
+++ ms.sql
@@ -1,15 +1,11 @@
 use firstdb;
 
-SELECT deptId FROM employee GROUP BY deptId;
+SELECT deptId,SUM(empSal) AS 'TOTAL SALARY'
+FROM employee
+GROUP BY deptId
+HAVING deptId=20;
 
-SELECT deptId,sum(empSal) AS 'total salary' FROM employee 
-GROUP BY deptId;
-
-SELECT deptId,max(empSal) AS 'Max salary' FROM employee 
-GROUP BY deptId;
-
-SELECT deptId,min(empSal) AS 'min salary' FROM employee 
-GROUP BY deptId;
-
-SELECT deptId,ROUND(AVG(empSal),2) AS 'AVG Salary' FROM employee
-GROUP BY deptId;
+SELECT deptId,SUM(empSal) AS 'TOTAL SALARY'
+FROM employee
+GROUP BY deptId
+HAVING SUM(empSal) BETWEEN 25000 AND 500000;
