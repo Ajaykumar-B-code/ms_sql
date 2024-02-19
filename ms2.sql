4a400b7b9606f9bcd06e3c19ac73ba895c7d2aef
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-19 17:21:01 +0530

    Add[Ajay] Alter command to add drop and modify the data type

--- ms2.sql
+++ ms2.sql
@@ -1,4 +1,9 @@
 SELECT * from emp_backup
 
 alter table emp_backup drop column locat2
+ALTER TABLE emp_backup ALTER COLUMN empName VARCHAR(30);
+
+SELECT * from emp_backup
+
+alter table emp_backup drop column locat2
  ALTER TABLE emp_backup ALTER COLUMN empName VARCHAR(30);
