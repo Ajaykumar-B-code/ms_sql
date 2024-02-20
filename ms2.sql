e842ae3bf1f2dccd7f7331945b1b05fd69ac30c8
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-20 10:50:34 +0530

    [Ajay] Add : subQueries in ms sql

--- ms2.sql
+++ ms2.sql
@@ -1,4 +1,5 @@
 
+use joins
 /*create database joins
 
 use joins
@@ -49,10 +50,23 @@ ON e.emp_deptid=d.dept_id;
 SELECT e.emp_id,e.emp_name,e.emp_sal,d.dept_name,d.dept_location
 FROM employee AS e
 RIGHT OUTER JOIN department AS d
-ON e.emp_deptid=d.dept_id;*/
+ON e.emp_deptid=d.dept_id;
 
 SELECT e.emp_id,e.emp_name,e.emp_sal,d.dept_name,d.dept_location
 FROM employee AS e
 FULL OUTER JOIN department AS d
-ON e.emp_deptid=d.dept_id;
+ON e.emp_deptid=d.dept_id;*/
 
+
+--SUB QUERIES
+
+SELECT emp_name,emp_sal FROM employee 
+WHERE emp_sal > (SELECT emp_sal FROM employee WHERE emp_name ='MIKE')
+
+SELECT emp_name,emp_sal FROM employee
+WHERE emp_sal>(SELECT emp_sal FROM employee WHERE emp_name='ADAM') 
+AND emp_deptid = (SELECT emp_deptId FROM employee WHERE emp_name ='ADAM')
+
+
+SELECT * FROM employee 
+WHERE emp_deptid = (SELECT dept_id FROM department WHERE dept_location='NEW YORK');
